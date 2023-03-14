c27c5d19d2a1397c681b0c225378c563d393a2e4
Kwan <kwan@Kwande-MacBook-Pro.local>
2023-03-15 03:59:44 +0800

    First commit of my website

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer>
+	        
+    <div class="container">
+        <div class='row align-items-center'>
+              <div class='text-left'>
+                  <img src='images/ku-logo-black.svg' width='85'>
+              </div>
+             
+              <div class='ml-auto'>
+                 
+                  <p>
+                    <i class="bi bi-telephone"></i>(852) 9528 6712
+                  </p>
+
+                  <p>		
+                    <i class="bi bi-envelope-at"></i><a href='mailto:xxx@gmail.com'>ksckuku@gmail.com</a>
+                 </p>
+              </div>
+        </div>
+    </div>	        
+
+</footer>
